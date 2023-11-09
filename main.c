32c061202f551f941e2d0ae35d1bdde3d1f395f0
subinkim32 <salgeum1222@gmail.com>
2023-11-09 16:01:31 +0900

    04

--- main.c
+++ main.c
@@ -5,12 +5,9 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char src[] = "The worst things to eat before you sleep";
-	char dst[100];
+	char src[30] = "happy C programming";
 	
-	strcpy(dst, src);
-	
-	printf("copied string: %s\n", dst);
-	
+	printf("string \"%s\" length: %d\n", src, strlen(src));
+		
 	return 0;
 }
